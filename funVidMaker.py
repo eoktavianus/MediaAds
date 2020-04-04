5ba1ccbd7405513fc38f1efdd8812c04cb116706
eoktavianus <eoktavianus@yahoo.com>
2020-04-05 01:01:20 +0700

    change storage to Azure

--- funVidMaker.py
+++ funVidMaker.py
@@ -11,6 +11,8 @@ from moviepy.editor import *
 from moviepy.video.fx.resize import resize
 from pygame import *
 
+dirInput = "C:/Users/eoktavianus/OneDrive - Agung Sedayu Group/MediaASG/PIK2 MATERIAL/MediaAdsApps/Input/"
+dirOutput = "C:/Users/eoktavianus/OneDrive - Agung Sedayu Group/MediaASG/PIK2 MATERIAL/MediaAdsApps/Output/"
 
 clips_array = []
 def vid_merge(clips, newname, nfps):
@@ -18,13 +20,16 @@ def vid_merge(clips, newname, nfps):
         clip = VideoFileClip(c)
         clips_array.append(clip)
         
-    nsize = (clips_array[0].w, clips_array[0].h)    
+    # nsize = (clips_array[0].w, clips_array[0].h)    
+    # nsize = (120,120)
     nclip = concatenate([cl for cl in clips_array], method="chain")
-    nclip.resize(nsize).write_videofile(newname, audio=True, fps=nfps)
+    nclip.resize(0.10).write_videofile(newname, audio=True, fps=nfps)
     nclip.close()
 
-nVidName = "merge01.mp4"
-vid_merge(["vid/MITRAPIK2.mp4","vid/asgcomp.mp4"], nVidName, 20)    
-prevVid = VideoFileClip(nVidName)
-prevVid.preview()
+nVidName = dirOutput + "ProfilePIK2Wtown.mp4"
+# vid_merge(["vid/MITRAPIK2.mp4","vid/asgcomp.mp4"], nVidName, 20)    
+vid_merge([dirInput + "02 VIDEO PROFILE PIK 2 (FURYCO) - ING - 2019-09-26.mp4", dirInput + "05WATERTOWN.mp4"], nVidName, 20)
+# prevVid = VideoFileClip(nVidName)
+# prevVid = VideoFileClip(dirInput + "02 VIDEO PROFILE PIK 2 (FURYCO) - ING - 2019-09-26.mp4")
+# prevVid.resize(0.10).preview()
 
